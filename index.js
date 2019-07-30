2d221819316541bbe8c28ac24501dffabca77d32
HenriqueTardivo <henrique.tardivo@gmail.com>
2019-07-30 20:23:34 -0300

    timer concluido

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ function timer() {
   var exercicio = document.getElementById("tempoExercicio").value;
   var descanso = document.getElementById("tempoDescando").value;
 
-  document.getElementById("tempo1").innerHTML = exercicio;
-  document.getElementById("tempo2").innerHTML = descanso;
-
-  //setInterval
+  var timer1 = setInterval(function() {
+    document.getElementById("tempo1").innerHTML =
+      exercicio + "  SEGUNDOS RESTANTES";
+    exercicio -= 1;
+    if (exercicio <= 0) {
+      document.getElementById("tempo1").innerHTML = "AGORA DESCANSA";
+      clearInterval(timer1);
+      setInterval(function() {
+        document.getElementById("tempo2").innerHTML =
+          descanso + "  SEGUNDOS DE DESCANSO RESTANTES";
+        descanso -= 1;
+        if (descanso <= 0)
+          document.getElementById("tempo2").innerHTML = "TEMPO ESGOTADO";
+      }, 1000);
+    }
+  }, 1000);
 }
