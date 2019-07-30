1fd79aaebb5d39256e474ff6e94cea2ca1ddc310
HenriqueTardivo <henrique.tardivo@gmail.com>
2019-07-30 16:36:23 -0300

    0.2

---
+++ index.js
@@ -0,0 +1,7 @@
+function timer() {
+  var exercicio = document.getElementById("tempoExercicio").value;
+  var descanso = document.getElementById("tempoDescando").value;
+
+  document.getElementById("tempo1").innerHTML = exercicio;
+  document.getElementById("tempo2").innerHTML = descanso;
+}
